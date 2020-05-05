aecef290decc471a673cfbe7d89108d80318093a
calebfavor <caleb@drumeo.com>
2020-05-05 14:50:33 -0700

    Adding more UI form elements. Fixing floating lables not working properly after input is entered. Adjusting postioning for floating labels.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,23 @@ module.exports = {
             '1/2': '50%',
             '3/4': '75%',
             'full': '100%',
+        },
+        borderWidth: {
+            default: '1px',
+            '0': '0',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '6': '6px',
+            '8': '8px',
+            '16': '16px',
         }
     },
     variants: {
         backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-        textColor: ['responsive', 'hover', 'focus', 'active']
+        textColor: ['responsive', 'hover', 'focus', 'active'],
+        boxShadow: ['responsive', 'hover', 'focus', 'active', 'focus-within'],
+        cursor: ['responsive', 'hover', 'focus'],
     },
     plugins: [],
 }
