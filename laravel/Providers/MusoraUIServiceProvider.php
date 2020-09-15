a7415fd047d1fbf7f4ed5f5b936a5985933b29d7
calebfavor <caleb@drumeo.com>
2020-09-14 18:33:27 -0700

    Adding laravel and laravel blade service provider.

---
+++ laravel/Providers/MusoraUIServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Railroad\MusoraUI\Providers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class MusoraUIServiceProvider extends ServiceProvider
+{
+    /**
+     * RailcontentServiceProvider constructor.
+     *
+     * @param Application $application
+     */
+    public function __construct(Application $application)
+    {
+        parent::__construct($application);
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../../blade', 'courier');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
